3c730be2ed83cf7867be086e3bf0dc5e3ddb9cbe
Siddharth Verma <siddharthverma1384@gmail.com>
2025-09-09 11:22:43 +0530

    m

---
+++ Verilog/HalfAdder_n_HalfSubtractor StructuralModeling/ha_n_hs_StructuralModeling.v
@@ -0,0 +1,24 @@
+module ha_n_hs_StructuralModeling(input a,b, output sum,carry,diff,borrow);
+  wire w1;
+  xor(sum,a,b);
+  xor(diff,a,b);
+  not(w1,a);
+  and(carry,a,b);
+  and(borrow,w1,b);
+endmodule
+
+module ha_n_hs_StructuralModeling_tb();
+  reg a,b;
+  wire sum,carry,diff,borrow;
+  ha_n_hs_StructuralModeling uut(a,b,sum,carry,diff,borrow);
+  initial
+  begin
+    a=0;b=0;
+    #100
+    a=0;b=1;
+    #100
+    a=1;b=0;
+    #100
+    a=1;b=1;
+  end
+endmodule
