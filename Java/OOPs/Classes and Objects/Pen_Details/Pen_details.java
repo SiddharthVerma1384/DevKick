f1e4be286a6a92835c099434b77c6d241c55aa79
Siddharth Verma <siddharthverma1384@gmail.com>
2025-09-15 09:43:23 +0530

    mmm

---
+++ Java/OOPs/Classes and Objects/Pen_Details/Pen_details.java
@@ -0,0 +1,27 @@
+package Pen_Details;
+import java.util.*;
+class Pen{
+    String color;
+    int tip;
+
+    void setColor(String newColor){
+        color = newColor;
+    }
+    void setTip(int newTip){
+        tip = newTip;
+    }
+    void display (){
+        System.out.println("Color of the pen is : " + color);
+        System.out.println("Tip size of the pen is : " + tip);
+    }
+}
+public class Pen_details{
+    public static void main(String args[]){
+        Scanner sc = new Scanner (System.in);
+        Pen p1 = new Pen();
+        p1.color = "Yellow";
+        p1.setColor("Blue");
+        p1.setTip(5);
+        p1.display();
+    }
+}
