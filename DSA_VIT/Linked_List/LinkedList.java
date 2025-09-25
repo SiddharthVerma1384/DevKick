f194ff0010389bfbaf4b102c299a7004cd88e225
Siddharth Verma <siddharthverma1384@gmail.com>
2025-09-26 01:02:05 +0530

    uuu

--- DSA_VIT/Linked_List/LinkedList.java
+++ DSA_VIT/Linked_List/LinkedList.java
@@ -8,6 +8,49 @@ public class LinkedList {
     private LinkedList(){
         this.size = 0;
     }
+
+    public Node getNode(int index){
+        Node node = head;
+        for (int i=0; i<index; i++){
+            node = node.next;
+        }
+        return node;
+    }
+
+    public int DeleteFirst(){
+        int value = head.value;
+        head = head.next;
+        if (head == null){
+            tail = null;
+        }
+        size --;
+        return value;
+    }
+
+    public int DeleteLast(){
+        if (size <= 1){
+            return DeleteFirst();
+        }
+        int value = tail.value;
+        Node secondLast =  getNode(size - 2);
+        secondLast = tail;
+        tail.next = null;
+        return value;
+    }
+
+    public int Delete(int index){
+        if (index == 0){
+            return DeleteFirst();
+        }
+        if (index == size - 1){
+            return DeleteLast();
+        }
+        Node prev = getNode(index - 1);
+        int value = prev.next.value;
+        prev.next = prev.next.next;
+        return value;
+    }
+
     public void Insert_First(int val){
         Node node = new Node(val);
         node.next = head;
@@ -16,6 +59,27 @@ public class LinkedList {
         if (tail == null){
             tail = head;
         }
+        size++;
+    }
+
+    public void Insert_Last(int val){
+        if(tail == null){
+            Insert_First(val);
+            return ;
+        }
+        Node node = new Node(val);
+        tail.next = node;
+        tail = node;
+        size++;
+    }
+
+    public void Insert(int val, int index){
+        Node temp = head;
+        for (int i=0; i<index; i++){
+            temp = temp.next;
+        }
+        Node node = new Node (val, temp.next);
+        temp.next = node;
         size++;
     }
 
@@ -45,6 +109,17 @@ public class LinkedList {
         list.Insert_First(7);
         list.Insert_First(9);
         list.Insert_First(18);
+        list.Insert_Last(33);
+        list.Insert_Last(23);
+        list.Insert(100, 3);
+
+        list.display();
+        System.out.println(list.DeleteFirst());
+        list.display();
+        System.out.println(list.DeleteLast());
+        list.display();
+        System.out.println(list.Delete(2));
+        list.display();
 
         list.display();
     }
