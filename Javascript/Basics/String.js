c4311568e3f2020dc4c6e50c5556a49d1e4f4302
Siddharth Verma <siddharthverma1384@gmail.com>
2026-01-11 12:15:01 +0530

    kmm

---
+++ Javascript/Basics/String.js
@@ -0,0 +1,12 @@
+let name="Siddharth Verma"
+let gitrepo=66
+console.log(`My name is ${name} and repo count is ${gitrepo}`);//i can edit this here only
+
+
+// `` -> Back ticks
+//String Interpolation
+
+//...use this as fstring concept .....
+const getName = new String('Sidd') // String Object
+console.log(getName[0]);
+console.log(getName.__proto__);
