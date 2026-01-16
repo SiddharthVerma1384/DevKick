50ae72ff19dddfec199580b6f8b74d97ec12581f
Siddharth Verma <siddharthverma1384@gmail.com>
2026-01-17 01:12:11 +0530

    mmm

---
+++ Javascript/Basics_03/functions.js
@@ -0,0 +1,13 @@
+// Functions  :- Making package of codes , holding set of instructions
+function SayMyName(){
+    console.log("S");
+    console.log("I");
+    console.log("D");
+    console.log("D");
+    
+}
+//SayMyName
+SayMyName()
+
+//without parenthesis is the reference of function while ,
+//withparenthesis -> execution
