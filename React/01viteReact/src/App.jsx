4a1f8be410889e26a510e0d912a8b73f155436a2
Siddharth Verma <siddharthverma1384@gmail.com>
2026-05-12 04:13:29 +0530

    understanding vite bundler

---
+++ React/01viteReact/src/App.jsx
@@ -0,0 +1,14 @@
+import Chai from "./chai"
+
+function App() {
+  const username = "Siddharth"
+
+  return (
+    <>
+    <h1>Chai aur react |  {username}</h1>
+    <Chai/> 
+    </> 
+  )
+}
+
+export default App
