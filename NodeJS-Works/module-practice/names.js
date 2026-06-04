207ff4dfbb6116dfe80182c6759df097bf3ac57b
Siddharth Verma <siddharthverma1384@gmail.com>
2026-06-04 15:50:45 +0530

    Learning Node JS

---
+++ NodeJS-Works/module-practice/names.js
@@ -0,0 +1,5 @@
+const x = 'Sid'
+
+
+
+module.exports = {x}
---
+++ NodeJS-Works/module-practice/usecase.js
@@ -0,0 +1,13 @@
+const y = require('./names')
+
+const SayHi = (name) => {
+    return (
+        `Hi, ${name} you are passed as an onject.`
+    )
+}
+
+// const {x} = y
+console.log(y)
+// console.log(SayHi(x))
+
+require('./mind-game-export')
